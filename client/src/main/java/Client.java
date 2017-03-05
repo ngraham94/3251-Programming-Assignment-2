5bd30771e3378618a18f358d24bcaa57c39e5185
Kelvin Chen <kelvin@kelvinchen.org>
2017-03-05 02:08:07 -0500

    Set up project structure

---
+++ client/src/main/java/Client.java
@@ -0,0 +1,8 @@
+import cs3251.Net;
+
+public class Client {
+    public static void main(String[] args) {
+        // Testing if multi-project build is working
+        System.out.println(Net.MSS);
+    }
+}
---
+++ lib/src/main/java/cs3251/Net.java
@@ -0,0 +1,6 @@
+package cs3251;
+
+public class Net {
+    // Max segment size in bytes
+    public static int MSS = 1000;
+}
